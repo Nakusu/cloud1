1589b9a99bea8aff31f801ddbbdbb8c58e162560
Nakusu <nakusu35@hotmail.fr>
2025-06-18 11:50:38 +0200

    [project] global ending

--- roles/docker-compose-install/files/app/images/wordpress/requirements/install.sh
+++ roles/docker-compose-install/files/app/images/wordpress/requirements/install.sh
@@ -2,4 +2,6 @@
 
 wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=supervisor --admin_password=$WP_PASSWORD --admin_email=$WP_ADMIN;
 
-wp theme install --allow-root https://downloads.wordpress.org/theme/twentytwentyone.2.5.zip --activate;
+if [ ! -d /var/www/wordpress/wp-content/themes/twentytwentyone/ ]; then
+    wp theme install --allow-root https://downloads.wordpress.org/theme/twentytwentyone.2.5.zip --activate;
+fi
